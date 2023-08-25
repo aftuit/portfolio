938be7392b1c3cb57f7c9b2157a94cdfefe59dbf
aftuit <farruxaktamov6@gmail.com>
2023-08-26 00:05:06 +0500

    bio

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="home-content d-flex justify-content-center align-items-center mx-auto h-100 overflow-hidden w-75">
+  <div
+    class="home-content d-flex justify-content-center align-items-center mx-auto h-100 overflow-hidden w-75"
+  >
     <div class="box row justify-content-between">
       <div class="col-xl-3 col-sm-12 col-12">
         <div class="avatar text-center mx-auto">
@@ -10,30 +12,50 @@
         <div class="details text-xl-start text-center">
           <h3 class="name">Farrukh Aktamov</h3>
           <p class="description text-white-50">
-            I am a Content creator, video producer and student from Uzbekistan
-            with strong background in filming/editing videos. And always looking
-            to work in a challenging and dynamic environment.
+            I'm a passionate Frontend Developer with a strong focus
+            on creating interactive and visually appealing user interfaces. With
+            2 years of experience in web development, I specialize in crafting
+            seamless experiences using cutting-edge technologies like React and
+            Vue.
           </p>
           <div class="social">
-            <ul class="nav justify-content-center justify-content-xl-start"> 
+            <ul class="nav justify-content-center justify-content-xl-start">
               <li class="nav-item">
                 <a href="https://github.com/aftuit" class="nav-link">
-                  <i class="fa-brands fa-github fa-xl" style="color: #ffffff;"></i>
+                  <i
+                    class="fa-brands fa-github fa-xl"
+                    style="color: #ffffff"
+                  ></i>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="https://www.linkedin.com/in/farrux-aktamov/" class="nav-link">
-                  <i class="fa-brands fa-linkedin fa-xl" style="color: #ffffff;"></i>
+                <a
+                  href="https://www.linkedin.com/in/farrux-aktamov/"
+                  class="nav-link"
+                >
+                  <i
+                    class="fa-brands fa-linkedin fa-xl"
+                    style="color: #ffffff"
+                  ></i>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="https://t.me/Farrux_Aktamov" class="nav-link">
-                  <i class="fa-brands fa-telegram fa-xl" style="color: #ffffff;"></i>
+                  <i
+                    class="fa-brands fa-telegram fa-xl"
+                    style="color: #ffffff"
+                  ></i>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="https://www.instagram.com/farrux__aktamov/" class="nav-link">
-                    <i class="fa-brands fa-instagram fa-xl" style="color: #ffffff;"></i>
+                <a
+                  href="https://www.instagram.com/farrux__aktamov/"
+                  class="nav-link"
+                >
+                  <i
+                    class="fa-brands fa-instagram fa-xl"
+                    style="color: #ffffff"
+                  ></i>
                 </a>
               </li>
             </ul>
@@ -44,7 +66,7 @@
   </div>
 </template>
 <script>
-import "./style.css"
+import "./style.css";
 export default {};
 </script>
 <style>
